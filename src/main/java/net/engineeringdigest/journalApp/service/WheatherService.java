81ce8a6a9b082df8d5a20757632a30a644c57996
Piyush <piyushahuja575@gmail.com>
2026-02-18 01:27:04 +0530

    Addding api key in application.yml

--- src/main/java/net/engineeringdigest/journalApp/service/WheatherService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/WheatherService.java
@@ -2,6 +2,7 @@ package net.engineeringdigest.journalApp.service;
 
 import net.engineeringdigest.journalApp.api.response.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -9,7 +10,9 @@ import org.springframework.web.client.RestTemplate;
 
 @Component
 public class WheatherService{
-    public static final String api_key="63e8d5347951a7b959f97c7d8c9d436f";
+
+    @Value("${weather.api.key}")
+    public   String api_key;
     public static final String API="https://api.weatherstack.com/current?access_key=API_KEY&query=CITY";
 
     @Autowired
